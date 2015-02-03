1ff5cfd909cfee41e9e3340bc7e7ea93c8a0202a
Ginseng <bitnick10@gmail.com>
2015-02-03 12:35:07 +0800

    Add Clean Cache Module

---
+++ mepy.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+def CleanCacheModules():
+    filenames = os.listdir("E:\\My\\Github\\mepy")
+    for filename in filenames:
+        m = os.path.splitext(filename)[0]
+        if m in sys.modules:
+            print "clean cache module " + m
+            del sys.modules[m]
