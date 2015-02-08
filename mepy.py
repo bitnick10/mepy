dd6e2f80400f23ff13fca38ccefb4c8cdb586bd3
Ginseng <bitnick10@gmail.com>
2015-02-08 11:50:55 +0800

    Bug Fixed

--- mepy.py
+++ mepy.py
@@ -2,8 +2,8 @@ import sys
 import os
 import math
 
-def CleanCacheModules():
-    filenames = os.listdir("E:\\My\\Github\\mepy")
+def CleanCacheModules(dir):
+    filenames = os.listdir(dir)
     for filename in filenames:
         m = os.path.splitext(filename)[0]
         if m in sys.modules:
